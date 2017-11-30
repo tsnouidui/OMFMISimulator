3d907654014f5e49c296255bbf5bec1ce33622c8
Lennart Ochel <lennart.ochel@liu.se>
2017-11-30 15:43:28 +0100

    Fix OMSimulatorPython.bat

--- src/OMSimulatorPython/OMSimulatorPython.bat
+++ src/OMSimulatorPython/OMSimulatorPython.bat
@@ -1,5 +1,6 @@
 @echo off
 REM %~dp0 will give the location of the bat file
-REM set Pythonpath to the location of python script
-set PYTHONPATH=%PYTHONPATH%;%~dp0../lib
+REM set PYTHONPATH to the location of python script
+set PYTHONPATH=%PYTHONPATH%;%~dp0..\lib
+set PATH=%PATH%;%~dp0..\lib
 python "%*"
